2ded3290839488a4163bf5611cf94d1a6ce01fd4
J0NGS <jnetogoncalo@gmail.com>
2023-03-07 16:42:35 -0300

    att CRUD FindByName

--- group04-api/src/main/java/com/group05/abstractbusiness/group04api/Controller/PersonController.java
+++ group04-api/src/main/java/com/group05/abstractbusiness/group04api/Controller/PersonController.java
@@ -1,12 +1,16 @@
 package com.group05.abstractbusiness.group04api.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.group05.abstractbusiness.group04api.Repository.PersonRepository;
 import com.group05.abstractbusiness.group04api.Model.Person;
@@ -19,12 +23,26 @@ public class PersonController {
     @Autowired
     private PersonRepository personRepository;
 
-    @GetMapping()
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.FOUND)
     public List<Person> listAll(){
         return personRepository.findAll();
     }
-    
-    @PostMapping
+
+    @GetMapping("/byId")
+    @ResponseStatus(HttpStatus.FOUND)
+    public Optional<Person> listById(@RequestBody Person person){
+        return personRepository.findById(person.getId());
+    }
+
+    @GetMapping("/byName")
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<Person> listByNameLike(@RequestParam("name") String name){
+        return personRepository.findByName("%"+ name + "%");
+    }
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
     public Person CreatePerson(@RequestBody Person person) {
         return personRepository.save(person);
     }
--- group04-api/src/main/java/com/group05/abstractbusiness/group04api/Repository/PersonRepository.java
+++ group04-api/src/main/java/com/group05/abstractbusiness/group04api/Repository/PersonRepository.java
@@ -1,10 +1,18 @@
 package com.group05.abstractbusiness.group04api.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.group05.abstractbusiness.group04api.Model.Person;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long>{
+    //Função para fazer busca de pessoa pelo nome
+    @Query("SELECT p FROM Person p WHERE translate(lower(p.name), 'áàãâéêíóôõúüç','aaaaeeiooouuc') LIKE %:name%")       /*Deixa tudo em minusculo e sem acento*/
+    public List<Person> findByName(@Param("name") String name);
+    
 }

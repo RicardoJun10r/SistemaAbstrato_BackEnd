bfb77621eb7ea463daabda1c0df51e8c5eff7ed9
J0NGS <jnetogoncalo@gmail.com>
2023-03-09 02:13:05 -0300

    att 0.1 customer

---
+++ group04-api/src/main/java/com/group05/abstractbusiness/model/Customer.java
@@ -0,0 +1,17 @@
+package com.group05.abstractbusiness.model;
+
+public class Customer extends Person{
+    //id
+    private Long id;
+
+    //Adress
+    private String adress;
+    
+    //email
+    private String email;
+
+    //number
+    private String number;
+    
+
+}

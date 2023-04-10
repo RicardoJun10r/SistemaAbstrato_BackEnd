08e8fbc219d91c68c4390c77b093bca6db8bc05b
Vitor Oliveira Ropke <vitorropke@hotmail.com>
2023-04-10 20:42:14 -0300

    Update Transaction.java

--- group04-api/src/main/java/com/group05/abstractbusiness/model/Transaction/Transaction.java
+++ group04-api/src/main/java/com/group05/abstractbusiness/model/Transaction/Transaction.java
@@ -7,12 +7,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-public class Transaction {
+@Data
+@EqualsAndHashCode
+@MappedSuperclass
+public abstract class Transaction {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)

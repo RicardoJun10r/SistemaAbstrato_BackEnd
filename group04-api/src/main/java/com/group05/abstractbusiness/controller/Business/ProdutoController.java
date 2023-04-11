4efddf5f0fca9b3a4e9984bd1834f8dc254e8f8b
RicardoJun10r <ricardojunior0110@gmail.com>
2023-04-10 21:30:35 -0300

    att

--- group04-api/src/main/java/com/group05/abstractbusiness/controller/Business/ProdutoController.java
+++ group04-api/src/main/java/com/group05/abstractbusiness/controller/Business/ProdutoController.java
@@ -1,13 +1,11 @@
 package com.group05.abstractbusiness.controller.Business;
 
-import java.net.URI;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.group05.abstractbusiness.model.Business.factory.*;
 import com.group05.abstractbusiness.model.Business.*;
@@ -40,8 +37,6 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoIntelectualService produtoIntelectualService;
-
-    private AbstractFactoryProduto fabrica;
 
     //#region CRIAR PRODUTOS OU SERVIÇOS
 

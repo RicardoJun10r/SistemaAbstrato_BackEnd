8774aee78a637765fe6f36a078d66e58d8af11af
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-04-24 21:24:36 -0300

    AttCartAndTransactionsModel

---
+++ group04-api/src/main/java/com/group05/abstractbusiness/modules/model/Cart.java
@@ -0,0 +1,74 @@
+package com.group05.abstractbusiness.modules.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import com.group05.abstractbusiness.modules.model.Business.ProdutoFisico;
+import com.group05.abstractbusiness.modules.model.Person.Customer;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapKeyJoinColumn;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode
+@Entity
+@Table(name = "cart")
+public class Cart {
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", unique = true)
+	private UUID id;
+
+    @ElementCollection
+    @CollectionTable(name = "cart_products", joinColumns = @JoinColumn(name = "cart_id"))
+    @MapKeyJoinColumn(name = "product_id")
+	@Column(name = "quantity")
+	private Map<ProdutoFisico, Integer> products;                           //Map para guardar produto e quantidade de produto no carrinho
+    //TO-DO Adicionar vendendor
+
+	public Cart(UUID id) {
+		this.id = id;
+    }
+
+    //Metodo que adiciona produto, se já existir no carrinho aumenta a quantidade
+    public void addProduct(ProdutoFisico product, int quantity) {
+        if (products.containsKey(product)) {
+            int currentQuantity = products.get(product);
+            products.put(product, currentQuantity + quantity);
+        } else {
+            products.put(product, quantity);
+        }
+    }
+    //Metodo que remove produto, se já existir no carrinho diminui a quantidade
+    public void removeProduct(ProdutoFisico product, int quantity) {
+        if (products.containsKey(product)) {
+            int currentQuantity = products.get(product);
+            if (quantity >= currentQuantity) {
+                products.remove(product);
+            } else {
+                products.put(product, currentQuantity - quantity);
+            }
+        }
+    }
+    //Metodo que retorna produtos no carrinho
+    public List<ProdutoFisico> getProducts() {
+        return new ArrayList<>(products.keySet());
+    }
+    //Metodo que retorna quantidade especifica de produtos no carrinho
+    public int getQuantity(ProdutoFisico product) {
+        return products.getOrDefault(product, 0);
+    }
+}

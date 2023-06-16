5fe8afa168502fd1f1a9f0f4cdb418236205ba1e
RicardoJun10r <ricardojunior0110@gmail.com>
2023-06-16 01:09:36 -0300

    feat: crud methods for digital and intelectual services

--- group04-api/src/main/java/com/group05/abstractbusiness/modules/service/Business/ProdutoDigitalService.java
+++ group04-api/src/main/java/com/group05/abstractbusiness/modules/service/Business/ProdutoDigitalService.java
@@ -3,14 +3,22 @@ package com.group05.abstractbusiness.modules.service.Business;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
+import com.group05.abstractbusiness.error.Exception.ResourceNotAcceptable;
+import com.group05.abstractbusiness.error.Exception.ResourceNotFoundException;
+import com.group05.abstractbusiness.helper.DTO.Business.Teste.ProductDigRes;
+import com.group05.abstractbusiness.helper.DTO.Business.Teste.ProductRes;
+import com.group05.abstractbusiness.modules.model.Business.Produto;
 import com.group05.abstractbusiness.modules.model.Business.ProdutoDigital;
+import com.group05.abstractbusiness.modules.model.Business.factory.ProdutoFactory;
 import com.group05.abstractbusiness.modules.repository.Business.ProdutoDigitalRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ProdutoDigitalService {
@@ -18,35 +26,150 @@ public class ProdutoDigitalService {
     @Autowired
     private ProdutoDigitalRepository produtoDigitalRepository;
 
-    public ProdutoDigital adicionar(ProdutoDigital produto){
-        return produtoDigitalRepository.save(produto);
+    private ModelMapper mapper = new ModelMapper();
+
+    @Transactional
+    public ProductRes createProduct(ProdutoFactory produtoFactory){
+        
+        verifyProduct(produtoFactory.getName());
+        
+        ProdutoDigital produtoDigital = produtoDigitalRepository.save(mapper.map(produtoFactory.criarFisco(), ProdutoDigital.class));
+        
+        return mapper.map(produtoDigital, ProductDigRes.class);
+
     }
 
-    public Optional<ProdutoDigital> buscar(UUID id){
-        Optional<ProdutoDigital> optional = produtoDigitalRepository.findById(id);
-        if(optional.isPresent()){
-            return optional;
-        } else {
-            return Optional.of(optional.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto digital [ " + id + " ] não econtrado.")));
-        }
+    private void verifyProduct(String nome){
+        Optional<ProdutoDigital> p = produtoDigitalRepository.findByName(nome);
+        if(!p.isPresent()) throw new ResourceNotAcceptable("Produto já existe");
     }
 
-    public List<ProdutoDigital> listar(){
-        List<ProdutoDigital> lista = produtoDigitalRepository.findAll();
-        if(lista.isEmpty()){
-            throw new RuntimeException("Nenhum produto encontrado!!! "); 
-        } else {
-            return lista;
+    public List<ProductRes> findAll(){
+        return produtoDigitalRepository.findAll().stream().map(
+            produto -> mapper.map(produto, ProductDigRes.class)
+        ).collect(Collectors.toList());
+    }
+
+    public ProductRes findById(UUID id){
+
+        Optional<ProdutoDigital> produto = produtoDigitalRepository.findById(id);
+        if(produto.isPresent()) return mapper.map(produto, ProductDigRes.class);
+        else throw new ResourceNotFoundException("Produto não encontrado!");
+
+    }
+
+    public ProductRes findByName(String name){
+
+        Optional<ProdutoDigital> produto = produtoDigitalRepository.findByName(name);
+        if(produto.isPresent()) return mapper.map(produto, ProductDigRes.class);
+        else throw new ResourceNotFoundException("Produto não encontrado!");
+
+    }
+
+    public List<ProductRes> findByBrand(String brand){
+ 
+        return produtoDigitalRepository.findByBrand(brand).stream().map(
+            produto -> mapper.map(produto, ProductDigRes.class)
+        ).collect(Collectors.toList());
+
+    }
+
+    public List<ProductRes> findByCategory(String category){
+
+        return produtoDigitalRepository.findByCategory(category).stream().map(
+            produto -> mapper.map(produto, ProductDigRes.class)
+        ).collect(Collectors.toList());
+
+    }
+
+    public List<ProductRes> findBySubcategory(String subcategory){
+
+        return produtoDigitalRepository.findBySubCategory(subcategory).stream().map(
+            produto -> mapper.map(produto, ProductDigRes.class)
+        ).collect(Collectors.toList());
+
+    }
+
+    @Transactional
+    public ProductRes updateProdutos(UUID id, ProdutoFactory produtoAtualizado){
+
+        Optional<ProdutoDigital> produtoDigital = this.produtoDigitalRepository.findById(id);
+
+        updateProduto(produtoDigital.get(), produtoAtualizado);
+
+        updateProdutoDigital(produtoDigital.get(), produtoAtualizado);
+
+        return mapper.map(this.produtoDigitalRepository.save(produtoDigital.get()), ProductDigRes.class);
+
+    }
+
+    private void updateProduto(Produto produto, ProdutoFactory produtoFactory){
+
+        if(produtoFactory.getName() != null && !produtoFactory.getName().isEmpty()){
+            produto.setName(produtoFactory.getName());
         }
+
+        if(produtoFactory.getDescription() != null && !produtoFactory.getDescription().isEmpty()){
+            produto.setDescription(produtoFactory.getDescription());
+        }
+
+        if(produtoFactory.getStatus() != null){
+            produto.setStatus(produtoFactory.getStatus());
+        }
+
+        if(produtoFactory.getCost() != null){
+            produto.setCost(produtoFactory.getCost());
+        }
+
+        if(produtoFactory.getPrice() != null){
+            produto.setPrice(produtoFactory.getPrice());
+        }
+
+        if(produtoFactory.getBrand() != null && !produtoFactory.getBrand().isEmpty()){
+            produto.setBrand(produtoFactory.getBrand());
+        }
+
+        if(produtoFactory.getCategory() != null && !produtoFactory.getCategory().isEmpty()){
+            produto.setCategory(produtoFactory.getCategory());
+        }
+
+        if(produtoFactory.getSubCategory() != null && !produtoFactory.getSubCategory().isEmpty()){
+            produto.setSubCategory(produtoFactory.getSubCategory());
+        }
+
+        if(produtoFactory.getImage() != null && !produtoFactory.getImage().isEmpty()){
+            produto.setImage(produtoFactory.getImage());
+        }
+
+        if(produtoFactory.getUpdatedAt() != null){
+            produto.setUpdatedAt(produtoFactory.getUpdatedAt());
+        }
+
+    }
+
+    private void updateProdutoDigital(ProdutoDigital produtoDigital, ProdutoFactory produto){
+
+        if(produto.getFileUrl() != null && !produto.getFileUrl().isEmpty()){
+            produtoDigital.setFileUrl(produto.getFileUrl());
+        }
+
+        if(produto.getFileType() != null && !produto.getFileType().isEmpty()){
+            produtoDigital.setFileType(produto.getFileType());
+        }
+
+        if(produto.getFileSize() != null){
+            produtoDigital.setFileSize(produto.getFileSize());
+        }
+
+        if(produto.getExpiryDate() != null){
+            produtoDigital.setExpiryDate(produto.getExpiryDate());
+        }
+
     }
 
     public String deletar(UUID id){
-        try {
-            produtoDigitalRepository.deleteById(id);
-            return "Produto Digital: [ " + id + " ] deletado";
-        } catch (Exception e) {
-            throw new RuntimeException("Produto de id = [ " + id + " ] não encontrado!!!");
-        }
+        produtoDigitalRepository.deleteById(id);
+        return "Produto Fisico: [ " + id + " ] deletado";
     }
 
 }
--- group04-api/src/main/java/com/group05/abstractbusiness/modules/service/Business/ProdutoIntelectualService.java
+++ group04-api/src/main/java/com/group05/abstractbusiness/modules/service/Business/ProdutoIntelectualService.java
@@ -3,12 +3,22 @@ package com.group05.abstractbusiness.modules.service.Business;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.group05.abstractbusiness.error.Exception.ResourceNotAcceptable;
+import com.group05.abstractbusiness.error.Exception.ResourceNotFoundException;
+import com.group05.abstractbusiness.helper.DTO.Business.Teste.ProductIntRes;
+import com.group05.abstractbusiness.helper.DTO.Business.Teste.ProductRes;
+import com.group05.abstractbusiness.modules.model.Business.Produto;
 import com.group05.abstractbusiness.modules.model.Business.ProdutoIntelectual;
+import com.group05.abstractbusiness.modules.model.Business.factory.ProdutoFactory;
 import com.group05.abstractbusiness.modules.repository.Business.ProdutoIntelectualRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ProdutoIntelectualService {
@@ -16,21 +26,154 @@ public class ProdutoIntelectualService {
     @Autowired
     private ProdutoIntelectualRepository produtoIntelectualRepository;
 
-    public ProdutoIntelectual adicionar(ProdutoIntelectual produto){
-        return produtoIntelectualRepository.save(produto);
+    private ModelMapper mapper = new ModelMapper();
+
+    @Transactional
+    public ProductRes createProduct(ProdutoFactory produtoFactory){
+        
+        verifyProduct(produtoFactory.getName());
+
+        ProdutoIntelectual produtoDigital = produtoIntelectualRepository.save(mapper.map(produtoFactory.criarFisco(), ProdutoIntelectual.class));
+
+        return mapper.map(produtoDigital, ProductIntRes.class);
+
     }
 
-    public Optional<ProdutoIntelectual> buscar(UUID id){
-        return produtoIntelectualRepository.findById(id);
+    private void verifyProduct(String nome){
+        Optional<ProdutoIntelectual> p = produtoIntelectualRepository.findByName(nome);
+        if(!p.isPresent()) throw new ResourceNotAcceptable("Produto já existe");
     }
 
-    public List<ProdutoIntelectual> listar(){
-        return produtoIntelectualRepository.findAll();
+    public List<ProductRes> findAll(){
+        return produtoIntelectualRepository.findAll().stream().map(
+            produto -> mapper.map(produto, ProductIntRes.class)
+        ).collect(Collectors.toList());
+    }
+
+    public ProductRes findById(UUID id){
+
+        Optional<ProdutoIntelectual> produto = produtoIntelectualRepository.findById(id);
+        if(produto.isPresent()) return mapper.map(produto, ProductIntRes.class);
+        else throw new ResourceNotFoundException("Produto não encontrado!");
+
+    }
+
+    public ProductRes findByName(String name){
+
+        Optional<ProdutoIntelectual> produto = produtoIntelectualRepository.findByName(name);
+        if(produto.isPresent()) return mapper.map(produto, ProductIntRes.class);
+        else throw new ResourceNotFoundException("Produto não encontrado!");
+
+    }
+
+    public List<ProductRes> findByBrand(String brand){
+ 
+        return produtoIntelectualRepository.findByBrand(brand).stream().map(
+            produto -> mapper.map(produto, ProductIntRes.class)
+        ).collect(Collectors.toList());
+
+    }
+
+    public List<ProductRes> findByCategory(String category){
+
+        return produtoIntelectualRepository.findByCategory(category).stream().map(
+            produto -> mapper.map(produto, ProductIntRes.class)
+        ).collect(Collectors.toList());
+
+    }
+
+    public List<ProductRes> findBySubcategory(String subcategory){
+
+        return produtoIntelectualRepository.findBySubCategory(subcategory).stream().map(
+            produto -> mapper.map(produto, ProductIntRes.class)
+        ).collect(Collectors.toList());
+
+    }
+
+    @Transactional
+    public ProductRes updateProdutos(UUID id, ProdutoFactory produtoAtualizado){
+
+        Optional<ProdutoIntelectual> produtoIntelectual = this.produtoIntelectualRepository.findById(id);
+
+        updateProduto(produtoIntelectual.get(), produtoAtualizado);
+
+        updateProdutoIntelectual(produtoIntelectual.get(), produtoAtualizado);
+
+        return mapper.map(this.produtoIntelectualRepository.save(produtoIntelectual.get()), ProductIntRes.class);
+
+    }
+
+    private void updateProduto(Produto produto, ProdutoFactory produtoFactory){
+
+        if(produtoFactory.getName() != null && !produtoFactory.getName().isEmpty()){
+            produto.setName(produtoFactory.getName());
+        }
+
+        if(produtoFactory.getDescription() != null && !produtoFactory.getDescription().isEmpty()){
+            produto.setDescription(produtoFactory.getDescription());
+        }
+
+        if(produtoFactory.getStatus() != null){
+            produto.setStatus(produtoFactory.getStatus());
+        }
+
+        if(produtoFactory.getCost() != null){
+            produto.setCost(produtoFactory.getCost());
+        }
+
+        if(produtoFactory.getPrice() != null){
+            produto.setPrice(produtoFactory.getPrice());
+        }
+
+        if(produtoFactory.getBrand() != null && !produtoFactory.getBrand().isEmpty()){
+            produto.setBrand(produtoFactory.getBrand());
+        }
+
+        if(produtoFactory.getCategory() != null && !produtoFactory.getCategory().isEmpty()){
+            produto.setCategory(produtoFactory.getCategory());
+        }
+
+        if(produtoFactory.getSubCategory() != null && !produtoFactory.getSubCategory().isEmpty()){
+            produto.setSubCategory(produtoFactory.getSubCategory());
+        }
+
+        if(produtoFactory.getImage() != null && !produtoFactory.getImage().isEmpty()){
+            produto.setImage(produtoFactory.getImage());
+        }
+
+        if(produtoFactory.getUpdatedAt() != null){
+            produto.setUpdatedAt(produtoFactory.getUpdatedAt());
+        }
+
+    }
+
+    private void updateProdutoIntelectual(ProdutoIntelectual produtoIntelectual, ProdutoFactory produto){
+
+        if(produto.getAuthor() != null && !produto.getAuthor().isEmpty()){
+            produtoIntelectual.setAuthor(produto.getAuthor());
+        }
+
+        if(produto.getPublisher() != null && !produto.getPublisher().isEmpty()){
+            produtoIntelectual.setPublisher(produto.getPublisher());
+        }
+
+        if(produto.getIsbn() != null && !produto.getIsbn().isEmpty()){
+            produtoIntelectual.setIsbn(produto.getIsbn());
+        }
+
+        if(produto.getPages() != null){
+            produtoIntelectual.setPages(produto.getPages());
+        }
+
+        if(produto.getEdition() != null){
+            produtoIntelectual.setEdition(produto.getEdition());
+        }
+
     }
 
     public String deletar(UUID id){
         produtoIntelectualRepository.deleteById(id);
-        return "Produto Intelectual: [ " + id + " ] deletado";
+        return "Produto Fisico: [ " + id + " ] deletado";
     }
     
 }

a832f4b282471ee1819f9e1e69a58dd1d263e816
RicardoJun10r <ricardojunior0110@gmail.com>
2023-06-19 00:59:47 -0300

    feat: customer pf and pj classes

---
+++ group04-api/src/main/java/com/group05/abstractbusiness/modules/model/Person/Customers/CustomerPF.java
@@ -0,0 +1,45 @@
+package com.group05.abstractbusiness.modules.model.Person.Customers;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.group05.abstractbusiness.modules.model.Person.IsupplierCustomer;
+import com.group05.abstractbusiness.modules.model.Person.Users.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customer_pf_tb")
+public class CustomerPF extends IsupplierCustomer {
+
+    // @CPF(message = "cheque o CPF")
+    @Column(name = "cpf", unique = true, nullable = false)
+    private String cpf;
+
+    @JsonBackReference
+    @ManyToOne
+    private User user;
+
+    public CustomerPF(UUID id, String name, String email, LocalDate registerDate, String address, String phone,
+            String cpf, User user) {
+        super(id, name, email, registerDate, address, phone);
+        this.cpf = cpf;
+        this.user = user;
+    }
+
+}
---
+++ group04-api/src/main/java/com/group05/abstractbusiness/modules/model/Person/Customers/CustomerPJ.java
@@ -0,0 +1,48 @@
+package com.group05.abstractbusiness.modules.model.Person.Customers;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.group05.abstractbusiness.modules.model.Person.IsupplierCustomer;
+import com.group05.abstractbusiness.modules.model.Person.Users.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customer_pj_tb")
+public class CustomerPJ extends IsupplierCustomer {
+
+    // @CNPJ(message = "cheque o CNPJ")
+    @Column(name = "cnpj", unique = true, nullable = false)
+    private String cnpj;
+
+    @Column(name = "company", nullable = false)
+    private String company;
+
+    @JsonBackReference
+    @ManyToOne
+    private User user;
+
+    public CustomerPJ(UUID id, String name, String email, LocalDate registerDate, String address, String phone,
+             String cnpj, String company, User user) {
+        super(id, name, email, registerDate, address, phone);
+        this.cnpj = cnpj;
+        this.company = company;
+        this.user = user;
+    }
+
+}

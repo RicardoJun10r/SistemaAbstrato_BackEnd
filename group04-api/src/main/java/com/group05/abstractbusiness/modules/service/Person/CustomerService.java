1df60abae9f953c1dba475ec5420fee0ee4931a7
RicardoJun10r <ricardojunior0110@gmail.com>
2023-06-19 00:56:45 -0300

    feat: add create methods

--- group04-api/src/main/java/com/group05/abstractbusiness/modules/service/Person/CustomerService.java
+++ group04-api/src/main/java/com/group05/abstractbusiness/modules/service/Person/CustomerService.java
@@ -1,72 +1,47 @@
-// package com.group05.abstractbusiness.modules.service.Person;
+package com.group05.abstractbusiness.modules.service.Person;
 
-// import org.modelmapper.ModelMapper;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.stereotype.Service;
-// import org.springframework.transaction.annotation.Transactional;
-// import org.springframework.web.server.ResponseStatusException;
-// import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-// import com.group05.abstractbusiness.helper.DTO.person.customer.CustomerDTO_PF;
-// import com.group05.abstractbusiness.helper.DTO.person.customer.CustomerReturn_PF;
-// import com.group05.abstractbusiness.modules.model.Person.Customer;
-// import com.group05.abstractbusiness.modules.repository.Person.CustomerRepository;
+import com.group05.abstractbusiness.error.Exception.ResourceBadRequest;
+import com.group05.abstractbusiness.helper.DTO.person.customer.CustomerRes;
+import com.group05.abstractbusiness.modules.model.Person.IPerson;
+import com.group05.abstractbusiness.modules.model.Person.Customers.CustomerFactory;
+import com.group05.abstractbusiness.utils.Enums.TipoCostumer;
+import com.group05.abstractbusiness.utils.Validator.EmailAndPasswordValidator;
 
-// @Service
-// public class CustomerService {
+@Service
+public class CustomerService {
     
-//     @Autowired
-//     private CustomerRepository repository;
+    @Autowired
+    private CustomerPFService customerPFService;
 
-//     private ModelMapper mapper = new ModelMapper();
+    @Autowired
+    private CustomerPJService customerPJService;
 
-//     public CustomerReturn_PF findbyId(UUID id){
-//         return mapper.map(
-//         (this.repository.findById(id).orElseThrow( 
-//             ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "user não econtrado pelo  id [ " + id + " ]"))), CustomerReturn_PF.class);
-//     }
+    @Transactional
+    public CustomerRes createCustomer(TipoCostumer tipo, CustomerFactory customerFactory) {
 
-//     public CustomerReturn_PF findbyCpf(String cpf){
-//         return mapper.map(this.repository.findByCpf(cpf).orElseThrow( 
-//             ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "user não econtrado pelo  id [ " + cpf + " ]")), CustomerReturn_PF.class);
-//     }
+        if(EmailAndPasswordValidator.verifyEmail(customerFactory.getEmail())){
+            if(tipo == TipoCostumer.PJ){
+                return customerPJService.createCustomerPJ(customerFactory.createCustomerPJ());
+            } else {
+                return customerPFService.createCustomerPF(customerFactory.createCustomerPF());
+            }
+        }
+        throw new ResourceBadRequest("Verifique o e-mail de customer!");
+    }
 
+    public IPerson findCustomerByEmail(TipoCostumer tipo, String email){
+        if(EmailAndPasswordValidator.verifyEmail(email)){
+            if(tipo == TipoCostumer.PJ){
+                return customerPJService.findCustomerPJ(email).get();
+            } else {
+                return customerPFService.findCustomerPF(email).get();
+            }
+        }
+        throw new ResourceBadRequest("Verifique o e-mail de customer!");
+    }
 
-//     public List<CustomerReturn_PF> findbyName(String name){
-//         if (this.repository.findByNameContainingIgnoreCase(name).isEmpty()){
-//             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhuma pessoa encontrada com o nome -> " + name + " " + Customer.class.getClass());
-//         }else{
-//             List<CustomerReturn_PF> suppliers = new ArrayList<>();
-//             for(int i = 0; i <  this.repository.findByNameContainingIgnoreCase(name).size();i++){
-//                 suppliers.add(i, mapper.map(this.repository.findByNameContainingIgnoreCase(name).get(i), CustomerReturn_PF.class));
-//             }
-//             return suppliers;
-//         }
-//     }
-
-//     @Transactional                                                              // Só persiste o dado caso passe todas as informações
-//     public Customer createCustomerPF(CustomerDTO_PF customer) {
-//         return this.repository.save(mapper.map(customer, Customer.class));
-//     }
-
-//     @Transactional
-//     public CustomerReturn_PF updateCustomerPF(UUID id,CustomerDTO_PF customer){
-//         Customer newObj = this.repository.findById(id).orElseThrow(
-//             ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Supplier com id [ " + id  + " ] não encontrado"));
-//         newObj.setName(customer.getName());
-//         newObj.setAddress(customer.getAddress());
-//         newObj.setEmail(customer.getEmail());
-//         newObj.setPhone(customer.getPhone());
-//         newObj.setCpf_cnpj(customer.getCpf());
-//         return mapper.map(this.repository.save(newObj), CustomerReturn_PF.class);
-//     } 
-
-//     public void deleteCustomerPF(UUID id){
-//         try {
-//             this.repository.deleteById(id);
-//         } catch (Exception e) {
-//             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Não é possivel excluir pois possui dados relacionados");
-//         }
-//     }
-// }
+}
--- group04-api/src/main/java/com/group05/abstractbusiness/modules/service/Person/SupplierService.java
+++ group04-api/src/main/java/com/group05/abstractbusiness/modules/service/Person/SupplierService.java
@@ -1,109 +1,49 @@
-// package com.group05.abstractbusiness.modules.service.Person;
+package com.group05.abstractbusiness.modules.service.Person;
 
-// import org.modelmapper.ModelMapper;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.stereotype.Service;
-// import org.springframework.transaction.annotation.Transactional;
-// import org.springframework.web.server.ResponseStatusException;
+import java.util.Optional;
 
-// import java.util.*;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-// import com.group05.abstractbusiness.error.Exception.ResourceNotFoundException;
-// import com.group05.abstractbusiness.helper.DTO.person.supplier.SupplierDTO;
-// import com.group05.abstractbusiness.helper.DTO.person.supplier.SupplierReturn;
-// import com.group05.abstractbusiness.modules.model.Person.Supplier;
-// import com.group05.abstractbusiness.modules.repository.Person.SupplierRepository;
+import com.group05.abstractbusiness.error.Exception.ResourceBadRequest;
+import com.group05.abstractbusiness.error.Exception.ResourceNotAcceptable;
+import com.group05.abstractbusiness.error.Exception.ResourceNotFoundException;
+import com.group05.abstractbusiness.helper.DTO.person.supplier.SupplierDTO;
+import com.group05.abstractbusiness.helper.DTO.person.supplier.SupplierRes;
+import com.group05.abstractbusiness.modules.model.Person.Suppliers.Supplier;
+import com.group05.abstractbusiness.modules.repository.Person.SupplierRepository;
+import com.group05.abstractbusiness.utils.Validator.EmailAndPasswordValidator;
 
-// @Service
-// public class SupplierService {
+@Service
+public class SupplierService {
     
-//     @Autowired
-//     private SupplierRepository repository;
+    @Autowired
+    private SupplierRepository supplierRepository;
 
-//     private ModelMapper mapper = new ModelMapper();
+    private ModelMapper mapper = new ModelMapper();
 
-//     public SupplierReturn findbyId(UUID id){
-//         return mapper.map(this.repository.findById(id).orElseThrow(
-//             ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "user não econtrado pelo  id ->" + id)), SupplierReturn.class);
-//     }
+    @Transactional
+    public SupplierRes createSupplier(SupplierDTO supplierDTO) {
 
-//     public List<SupplierReturn> findbyName(String name){
-//         if (this.repository.findByNameContainingIgnoreCase(name).isEmpty()){
-//             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pessoa não encontrada " + name + " " + Supplier.class.getClass());
-//         }else{
-//             List<SupplierReturn> suppliers = new ArrayList<>();
-//             for(int i = 0; i <  this.repository.findByNameContainingIgnoreCase(name).size();i++){
-//                 suppliers.add(i, mapper.map(this.repository.findByNameContainingIgnoreCase(name).get(i), SupplierReturn.class));
-//             }
-//             return suppliers;
-//         }
-//     }
+        if(EmailAndPasswordValidator.verifyEmail(supplierDTO.getEmail())){
+            if(supplierRepository.findByEmail(supplierDTO.getEmail()).isPresent()){
+                throw new ResourceNotAcceptable("Supplier já existe!");
+            }
+            return mapper.map(supplierRepository.save(mapper.map(supplierDTO, Supplier.class)), SupplierRes.class);
+        }
+        throw new ResourceBadRequest("Verifique o e-mail de supplier!");
+    }
 
-//     public List<SupplierReturn> findbyEmail(String email){
-//         if (this.repository.findByEmail(email).isEmpty()){
-//             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pessoa não encontrada ");
-//         }else{
-//             List<SupplierReturn> suppliers = new ArrayList<>();
-//             for(int i = 0; i <  this.repository.findByEmail(email).size();i++){
-//                 suppliers.add(i, mapper.map(this.repository.findByEmail(email).get(i), SupplierReturn.class));
-//             }
-//             return suppliers;
-//         }
-//     }
-
-//     public Supplier findSupplierByEmail(String email){
+    public Optional<Supplier> findSupplierByEmail(String email){
         
-//         List<Supplier> suppliers = this.repository.findAll();
+        Optional<Supplier> supplier = supplierRepository.findByEmail(email);
 
-//         Iterator<Supplier> iterator = suppliers.iterator();
+        if(supplier.isPresent()) return supplier;
 
-//         while (iterator.hasNext()) {
-//             Supplier supplier = iterator.next();
-//             if(supplier.getEmail().equals(email)) return supplier;
-//         }
+        throw new ResourceNotFoundException("Supplier not found!");
 
-//         throw new ResourceNotFoundException("Supplier not found");
+    }
 
-//     }
-
-//     public List<SupplierReturn> findbyPhone(String phone){
-//         if (this.repository.findByPhone(phone).isEmpty()){
-//             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pessoa não encontrada ");
-//         }else{
-//             List<SupplierReturn> suppliers = new ArrayList<>();
-//             for(int i = 0; i <  this.repository.findByPhone(phone).size();i++){
-//                 suppliers.add(i, mapper.map(this.repository.findByPhone(phone).get(i), SupplierReturn.class));
-//             }
-//             return suppliers;
-//         }
-//     }
-
-//     @Transactional                                                              // Só persiste o dado caso passe todas as informações
-//     public Supplier createSupplier(SupplierDTO supplier) {
-//         if(repository.findByEmail(supplier.getEmail()).isEmpty()){
-//             return this.repository.save(mapper.map(supplier, Supplier.class));
-//         }else{
-//             throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED, "Supplier já existente com esse email");
-//         }
-//     }
-
-//     @Transactional
-//     public SupplierReturn updateSupplier(UUID id, SupplierDTO supplier){
-//         Supplier newObj = this.repository.findById(id).orElseThrow(
-//             ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Supplier com id [ " + id  + " ] não encontrado"));
-//         newObj.setName(supplier.getName());
-//         newObj.setAddress(supplier.getAddress());
-//         newObj.setEmail(supplier.getEmail());
-//         newObj.setPhone(supplier.getPhone());
-//         return mapper.map(this.repository.save(newObj), SupplierReturn.class);
-//         } 
-
-//     public void deleteSupplier(UUID id){
-//         try {
-//             this.repository.deleteById(id);
-//         } catch (Exception e) {
-//             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Não é possivel excluir pois possui dados relacionados");
-//         }
-//     }
-// }
+}

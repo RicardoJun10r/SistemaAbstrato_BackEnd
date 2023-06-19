601404718a362782b04c2047733013853e9dabb6
RicardoJun10r <ricardojunior0110@gmail.com>
2023-06-19 00:57:10 -0300

    feat: adding variables to supplier dto class

--- group04-api/src/main/java/com/group05/abstractbusiness/helper/DTO/person/supplier/SupplierDTO.java
+++ group04-api/src/main/java/com/group05/abstractbusiness/helper/DTO/person/supplier/SupplierDTO.java
@@ -1,11 +1,28 @@
 package com.group05.abstractbusiness.helper.DTO.person.supplier;
 
+import java.time.LocalDate;
+
+import com.group05.abstractbusiness.modules.model.Person.Users.User;
+
 import lombok.Data;
 
 @Data
 public class SupplierDTO {
+
     String name;
-    String address;
+    
     String email;
+
+    LocalDate registerDate;
+    
+    String address;
+    
     String phone;
+
+    Double price;
+
+    LocalDate delivery;
+
+    User user;
+
 }
